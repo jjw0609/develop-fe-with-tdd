75b1f36802edc83f2acea13149debbb35067fdc6
Jung JaeWoong <jjw_0609@naver.com>
2024-03-20 21:06:48 +0900

    클릭카운트 모듈 - 스펙1

--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,4 +1,4 @@
-describe('App.ClickCountView 모듈', () => {
+describe('App.ClickCountView', () => {
     let udpateEl, clickCounter, view
 
     beforeEach(()=> {
@@ -7,9 +7,16 @@ describe('App.ClickCountView 모듈', () => {
         view = App.ClickCountView(clickCounter, updateEl)
     })
 
+    it('clickCounter를 주입하지 않으면 에러를 던진다', () => {
+        const clickCounter = null;
+        const updateEl = document.createElement('span');
+
+        const actual = () => App.ClickCountView(clickCounter, updateEl);
+        expect(actual()).toThrowError();
+    })
 
     describe('updateView()', () => {
-        it('ClickCounter의 getValue() 실행결과를 출력한다', ()=> {
+        it('ClickCounter의 getValue() 값을 출력한다', ()=> {
             const counterValue = clickCounter.getValue()
             view.updateView()
             expect(updateEl.innerHTML).toBe(counterValue.toString())
